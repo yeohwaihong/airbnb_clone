131247e3a1c31ea2a22111309380b5d35816e6d0
yeohwaihong <yeohwaihong@gmail.com>
2017-05-25 16:14:43 +0800

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  include Clearance::User
+  has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+      user = User.create!(name: auth_hash["name"], email: auth_hash["extra"]["raw_info"]["email"])
+      user.authentications << (authentication)      return user
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+  root "welcome#home"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
